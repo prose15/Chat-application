627b25fecd81fa079e8cba3e631bf39a6ab52fd6
prose15 <prosemartial@gmail.com>
2024-03-11 16:11:36 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,43 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectToMongoDB from './db/connectToMongoDB.js';
+import cookieParser from "cookie-parser";
+import authRoutes from './routes/auth.routes.js';
+import messageRoutes from './routes/message.routes.js';
+import userRoutes from './routes/user.router.js';
+// import { app, server } from "./socket/socket.js";
+
+
+dotenv.config();
+// const __dirname = path.resolve();
+const app = express()
+
+const PORT = process.env.PORT || 5000;
+
+
+
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+// app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+// app.get("*", (req, res) => {
+// 	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+// });
+
+// server.listen(PORT, () => {
+// 	connectToMongoDB();
+// 	console.log(`Server Running on port ${PORT}`);
+// });
+app.get("/",(req,res)=>{
+    res.send("Hello world")
+})
+
+app.listen(PORT, ()=> {
+    connectToMongoDB();
+    console.log(`Server running on port ${PORT}`);
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { Home } from "./pages/home/Home"
+
+
+function App() {
+
+  return (
+    <div className='p-4 h-screen flex items-center justify-center'>
+    <Home/>
+    </div>
+  )
+}
+
+export default App
