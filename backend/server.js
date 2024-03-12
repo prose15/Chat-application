8de71a434aeb41a24a89ee3f3b57c696f1bbe520
prose15 <prosemartial@gmail.com>
2024-03-12 22:44:02 +0530

    Final Commit

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import dotenv from "dotenv";
 import connectToMongoDB from './db/connectToMongoDB.js';
 import cookieParser from "cookie-parser";
@@ -13,7 +14,7 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-
+const __dirname = path.resolve();
 
 app.use(express.json()); 
 app.use(cookieParser());
@@ -22,6 +23,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
 	connectToMongoDB();
