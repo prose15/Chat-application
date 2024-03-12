4a2e8ebb5a24a1befb84f1b171cb64f7154b65e5
prose15 <prosemartial@gmail.com>
2024-03-12 23:16:25 +0530

    Small bug fixed

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-application-gnzb.onrender.com/", {
+			const socket = io("https://chat-application-gnzb.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
